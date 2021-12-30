6283ea3eb6bb773e85b43274fdc9587e6fc8de62
Alexandre Vrla <alexandre.vrla@gmail.com>
2021-12-30 20:44:14 +0100

    authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
             length: {maximum: 255},
             format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
   has_secure_password
+  validates :password, presence: true, allow_blank: false, length: {minimum: 6}
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -58,4 +58,14 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_equal @user.email, @user.reload.email.downcase
   end
+
+  test 'password should be present' do
+    @user.password = @user.password_confirmation = '' * 6
+    assert_not @user.valid?
+  end
+
+  test 'password should have a minimum length' do
+    @user.password = @user.password_confirmation = 'r' * 5
+    assert_not @user.valid?
+  end
 end
