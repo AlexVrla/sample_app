a4bf347af74f3babeea5660b05200ebcc9e8349b
Alexandre Vrla <alexandre.vrla@gmail.com>
2022-01-21 13:15:33 +0100

    new migration for remember user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,15 @@ class User < ApplicationRecord
   validates :password, presence: true, allow_blank: false, length: {minimum: 6}
 
 #returns the hash digest of the given string.
-
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  #returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
 end
