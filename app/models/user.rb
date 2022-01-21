77039ca4138c566b1fa2713c5cfb986bef1cce95
Alexandre Vrla <alexandre.vrla@gmail.com>
2022-01-21 14:49:59 +0100

    refacto User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { email.downcase! }
   validates :name, presence: true, length: {maximum: 50}
   validates :email, uniqueness: true, presence: true,
@@ -7,16 +8,23 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, allow_blank: false, length: {minimum: 6}
 
-#returns the hash digest of the given string.
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
+  class << self
+    # Returns the hash digest of the given string.
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
 
-  #returns a random token.
-  def User.new_token
-    SecureRandom.urlsafe_base64
-  end
+    # Returns a random token.
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
 
+    # Remembers a user in the db for use in persistent sessions.
+    def remember
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+    end
+  end
 end
