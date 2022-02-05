90aa0b210af731c128965ddd6ccc79d3a2870669
Alexandre Vrla <alexandre.vrla@gmail.com>
2022-02-05 23:30:28 +0100

    10.14 done

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:jaydee)
+  end
+
+  test "unsuccessful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { name: "",
+                                                email: "dude@beats",
+                                                password: "beats",
+                                                password_confirmation: "beat" }}
+    assert_template 'users/edit'
+    assert_select 'div.alert', "The form contains 4 errors."
+  end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { name: "J Dilla the Greatest",
+                                                email: "jdilla@gmail.com",
+                                                password: "beatsbeatsbeats",
+                                                password_confirmation: "beatsbeatsbeats" }}
+    assert_not flash.empty?
+    assert_redirected_to user_path(@user)
+    follow_redirect!
+    assert_template 'users/show'
+    @user.reload
+    assert_equal @user.name, "J Dilla the Greatest"
+    assert_equal @user.email, "jdilla@gmail.com"
+  end
+end
