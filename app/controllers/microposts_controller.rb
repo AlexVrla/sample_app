8457a80d7552849b925276ccbfe88a89754c8d70
Alexandre Vrla <alexandre.vrla@gmail.com>
2022-02-08 00:05:22 +0100

    CD microposts 13.3

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,34 @@
 class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:destroy, :create]
+
+  def create
+    @user = current_user
+    @micropost = @user.microposts.build(micropost_params)
+    if @micropost.save
+      redirect_to user_path(@user)
+    else
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+    @user = current_user
+    @micropost.destroy
+    flash[:danger] = "Please log in."
+    redirect_to user_path(@user)
+  end
+
+
+  private
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_path
+      end
+    end
 end
--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:jaydee)
+    @micropost = microposts(:louder)
+  end
+
+  test "should redirect create when not logged in" do
+    # assert_not is_logged_in?
+    assert_no_difference "Micropost.count" do
+      post microposts_path, params: { micropost: { content: "Lorem ipsum" } }
+    end
+    assert_redirected_to login_path
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference "Micropost.count" do
+      delete microposts_path(@micropost)
+    end
+    assert_redirected_to login_path
+  end
 end
